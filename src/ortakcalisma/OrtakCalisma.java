a4158ef1f5bc18ac8e24424ed41f6b1aed364b80
nildembay <74560545+nildembay@users.noreply.github.com>
2020-11-19 22:48:25 +0300

    Update OrtakCalisma.java

--- src/ortakcalisma/OrtakCalisma.java
+++ src/ortakcalisma/OrtakCalisma.java
@@ -5,6 +5,7 @@ public class OrtakCalisma {
 	public static void main(String[] args) {
 		System.out.println("09:48 - Selamlar");
 		System.out.println("nilufer brach inde yazildi 10:40 pm");
+		System.out.println("masterda yazildi 10:48 pm");
 
 	}
 
