e7cf2f0c0c5f12c3560bf30b3b66c956b2bea57f
nildembay <nildembay@gmail.com>
2020-11-19 22:41:20 +0300

    ilk commit

--- src/ortakcalisma/OrtakCalisma.java
+++ src/ortakcalisma/OrtakCalisma.java
@@ -4,6 +4,7 @@ public class OrtakCalisma {
 
 	public static void main(String[] args) {
 		System.out.println("09:48 - Selamlar");
+		System.out.println("nilufer brach inde yazildi 10:40 pm");
 
 	}
 
